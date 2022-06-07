9c920d2293b0eb6d5a595770a0a9f837511a9175
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-06-07 07:39:01 -0300

    Commit do exercício

---
+++ src/Exercicio/Exercicio6_2.java
@@ -0,0 +1,53 @@
+package Exercicio;
+
+import java.util.Scanner;
+
+public class Exercicio6_2 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int v[] = new int[10];
+        String opcoes[] = {"Quantidade de numeros pares e impares.", "Quantidade de numeros primos.",
+                "Quantidade de numeros positivos e negativos.",
+                "Quantidade de numeros multiplos de um determinado numero.",
+                "O maior e menor valor presente no vetor." ,
+                "verificar se existem numeros duplicados."};
+        System.out.println("Vetor Gerado:");
+        for (int i = 0; i < v.length; i++) {
+            v[i] = (int) (-11 + Math.random() * 22);
+            System.out.print(v[i] + " ");
+        }
+
+        System.out.println("\n--------------------------");
+        System.out.println("Tabela de opcoes");
+        System.out.println("--------------------------");
+        for(int i = 0; i<opcoes.length; i++){
+            System.out.printf("%d - %s\n",i+1,opcoes[i]);
+        }
+        System.out.println("--------------------------");
+        System.out.println("Sua opcao: ");
+        int resp = ler.nextInt();
+
+        switch(resp){
+            case 1:
+                System.out.printf("A quantidade de numeros pares e %d e de impares e %d",Metodo.par(v),v.length - Metodo.par(v));
+                break;
+            case 2:
+                System.out.printf("No total tem %d numeros primos no vetor.",Metodo.primo(v));
+                break;
+            case 3:
+                System.out.printf("Sao %d numeros positivos e %d negativos.",Metodo.positivo(v),Metodo.negativo(v));
+                break;
+            case 4:
+                System.out.println("Qual número deseja verificar? ");
+                int y = ler.nextInt();
+                System.out.printf("Existem %d numeros multiplos de %d dentro do vetor.",Metodo.multiplo(y,v),y);
+                break;
+            case 5:
+                System.out.printf("O maior numero e %d e o menor e %d", Metodo.maior(v),Metodo.menor(v));
+                break;
+            case 6:
+                Metodo.duplicado(v);
+                break;
+        }
+    }
+}
---
+++ src/Exercicio/Metodo.java
@@ -0,0 +1,96 @@
+package Exercicio;
+
+public class Metodo {
+    public static int par(int v[]){
+        int count = 0;
+        for(int x: v) if(x % 2 == 0) count++;
+        return count;
+    }
+
+    public static int maior(int v[]){
+        int maior = v[0];
+        for(int i = 0; i < v.length;i++){
+            if(v[i] > maior){
+                maior = v[i];
+            }
+        }
+        return maior;
+    }
+
+    public static int menor(int v[]){
+        int menor = v[0];
+        for(int i = 0; i < v.length;i++){
+            if(v[i] < menor){
+                menor = v[i];
+            }
+        }
+        return menor;
+    }
+
+    public static int positivo(int v[]){
+        int positivo=0;
+        for(int x:v) if(x >= 0) positivo++;
+        return positivo;
+    }
+
+    public static int negativo(int v[]){
+        int negativo = 0;
+        for(int x:v) if(x <0) negativo ++;
+        return negativo;
+    }
+
+    public static int primo(int v[]){
+        boolean validacao;
+        int count =0;
+        for (int x :v ) {
+            validacao = true;
+            if(x < 0) x *= (-1);
+
+            if(x == 0 || x == 1) validacao = false;
+
+            for (int y = 2; y < x; y++) {
+                if (x % y == 0) {
+                    validacao = false;
+                    break;
+                }
+            }
+            if(validacao) count++;
+        }
+        return count;
+    }
+
+    public static int multiplo(int x, int v[]){
+        int divisivel=0;
+        for(int z: v){
+            if(z % x == 0){
+                divisivel++;
+            }
+        }
+        return divisivel;
+    }
+
+    public static void duplicado(int v[]){
+        int count = 0;
+        int igual[] = new int[v.length];
+        for(int x = 0; x<v.length; x++){
+            for(int p = 0; p<v.length; p++){
+                if(x == p) break;
+                if(v[x] == v[p]) count++;
+            }
+        }
+        for(int i = 0 ; i<v.length; i++){
+            ++igual[(v[i])];
+        }
+        System.out.println("Numeros repetidos /// quantidade");
+        for(int i = 0 ; i<v.length; i++){
+            if(igual[i]>1){
+                System.out.println(" " + i + "......" + igual[i]);
+            }
+        }
+        if(count !=0)
+            System.out.printf("Existe numeros duplicados");
+        else
+            System.out.println("Nao existe numeros duplicados");
+
+    }
+}
